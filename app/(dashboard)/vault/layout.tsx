1432eb42e6e7e22a35a30f9c901291025af1adc0
hanzy-dev <j.farhan.muhammad@gmail.com>
2026-03-04 22:23:13 +0700

    feat(auth): fetch user in vault layout

--- app/(dashboard)/vault/layout.tsx
+++ app/(dashboard)/vault/layout.tsx
@@ -1,14 +1,18 @@
 import { Container } from "@/components/app/container";
 import { TopNav } from "@/components/app/top-nav";
+import { createSupabaseServerClient } from "@/lib/supabase/server";
 
-export default function DashboardLayout({
+export default async function VaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createSupabaseServerClient();
+  const { data } = await supabase.auth.getUser();
+
   return (
     <>
-      <TopNav />
+      <TopNav userEmail={data.user?.email} />
       <Container>{children}</Container>
     </>
   );
